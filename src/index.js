fe1c25cc789959fc731fdd1ce24b93905f324321
Daniel Pintado Várez <dapinva95@gmail.com>
2024-05-30 21:19:26 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-/* The code snippet provided is a JavaScript file that is importing the `app` module from `app.js` and
-the `connectDB` function from `db.js`. It then sets the `port` variable to 4000 and calls the
-`connectDB` function to establish a connection to the database. Finally, it starts the server using
-the `app.listen` method on the specified port and logs a message to the console indicating that the
-server is listening on that port. */
+/* El fragmento de código proporcionado es un archivo JavaScript que importa el módulo `app` desde `app.js` y
+la función `connectDB` de `db.js`. Luego establece la variable `port` en 4000 y llama al
+Función `connectDB` para establecer una conexión a la base de datos. Finalmente, inicia el servidor usando
+el método `app.listen` en el puerto especificado y registra un mensaje en la consola indicando que el
+El servidor está escuchando en ese puerto. */
 
 import app   from "./app.js";
 import {connectDB} from "./db.js";
